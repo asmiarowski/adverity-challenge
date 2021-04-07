10c8e9103383382c70120a895a8abb7c316690e7
Artur Smiarowski <artur.smiarowski@schange.com>
2021-04-07 10:30:48 +0200

    Initial commit - generated Spring Boot

---
+++ src/main/java/com/example/adveritychallenge/AdverityChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.adveritychallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdverityChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdverityChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/adveritychallenge/AdverityChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.adveritychallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdverityChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
